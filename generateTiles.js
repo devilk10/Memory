39cb071a18a6473e4fef144519f91d90e1c21aa1
ketansa <ketansa@in-ketansa.local>
2017-12-24 01:38:31 +0530

    Winning and loosing condition and timer added

--- generateTiles.js
+++ generateTiles.js
@@ -32,74 +32,118 @@ Game.prototype.shuffle = function(list) {
 
 let game = new Game();
 let cellIdCounter = 1;
-let turn = 0;
-let prevClick='';
+let turn = 1;
+let prevClick = '';
+let prevPos = '';
 let size = 4;
-let timeout = size*10*1000;
 let moves = [];
+let timer = 0;
+let countDown = size * size * 2;
 
 const generateTable = function(size) {
   let table = document.getElementById('grid');
   for (let rowCounter = 0; rowCounter < size; rowCounter++) {
-    let row = document.createElement('tr');
-    for (let colCounter = 0; colCounter < size; colCounter++) {
-      let cell = document.createElement('td');
-      cell.id = cellIdCounter++;
-      row.appendChild(cell);
-    }
-    table.appendChild(row);
+    generateRows(table);
   };
   return table;
 };
 
+let generateRows = function (table) {
+  let row = document.createElement('tr');
+  for (let colCounter = 0; colCounter < size; colCounter++) {
+    let cell = document.createElement('td');
+    cell.id = cellIdCounter++;
+    row.appendChild(cell);
+  }
+  table.appendChild(row);
+};
+
 let openCell = function() {
   showContent(game.list);
-  var cells = document.getElementById("grid");
-  cells.addEventListener('click', function() {
-    let cellId = event.target.id;
-    if (!isNaN(+cellId)) {
-      isCorrect(cellId);
-    }
-  });
+  setTimer();
+  let cells = document.getElementById("grid");
+  cells.addEventListener('click', reactOnClick);
+};
+
+let reactOnClick = function() {
+  let cellId = event.target.id;
+  if (!isNaN(+cellId)) {
+    isCorrect(cellId);
+  }
+};
+
+let setTimer = function() {
+  clearInterval(timer);
+  timer = setInterval(function() {
+    startTimer()
+  }, 1000);
+}
+
+let startTimer = function() {
+  if (countDown >= 0 && turn <= size * size) {
+    document.getElementById("timerBlock").innerHTML = countDown;
+    countDown--;
+  } else
+    gameOverAction()
+};
+
+let gameOverAction = function() {
+  document.getElementById('timerBlock').style.visibility = 'hidden';
+  let cells = document.getElementById("grid");
+  cells.removeEventListener('click',reactOnClick);
+  showMessage();
+}
+
+let showMessage = function () {
+  if (moves.length == size * size) {
+    document.getElementById("WinningMessage").innerHTML = "You won!";
+    return;
+  }
+  document.getElementById("WinningMessage").innerHTML = "You Loose!";
 };
 
 let isCorrect = function(cellId) {
   let number = game.list[+cellId - 1];
-  if (prevClick=='') {
-    prevClick = number;
-    updateOnCell(cellId,number);
-  } else if (number == prevClick) {
-    prevClick='';
-    updateOnCell(cellId,number);
-  } else if(!moves.includes(cellId)&&prevClick!=''){
+  if (prevClick == '') {
+    registerNumber(number,cellId);
+  } else if (number == prevClick && prevPos!=cellId) {
+    prevClick = '';
+    updateOnCell(cellId, number);
+  } else if (!moves.includes(cellId) && prevClick != '') {
     document.getElementById(cellId).innerText = number;
     blink(cellId);
   }
 };
 
-let updateOnCell = function (cellId,number) {
+let registerNumber = function (number,cellId) {
+  prevClick = number;
+  prevPos = cellId;
+  updateOnCell(cellId, number);
+};
+
+let updateOnCell = function(cellId, number) {
   document.getElementById(cellId).innerText = number;
   turn++;
   moves.push(cellId);
 }
 
-let blink = function (cellId) {
+let blink = function(cellId) {
   setTimeout(function() {
     document.getElementById(cellId).innerText = '';
   }, 100);
 }
 
-let showContent = function (list) {
-  for (var i = 1; i < list.length+1; i++) {
-    document.getElementById(i).innerText = list[i-1];
+let showContent = function(list) {
+  for (var i = 1; i < list.length + 1; i++) {
+    document.getElementById(i).innerText = list[i - 1];
   }
-  setTimeout(function () {
+  setTimeout(function() {
     resetTable(list);
-  },size*1000*1.5);
+  }, size * 1000);
 };
 
-let resetTable = function (list) {
-  for (var i = 1; i < list.length+1; i++) {
+let resetTable = function(list) {
+  for (var i = 1; i < list.length + 1; i++) {
     document.getElementById(i).innerText = '';
   }
 };
